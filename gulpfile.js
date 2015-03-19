33f0a95aa1eb2a2d62918329ce977fcb73c2f25f
Tien Le <tien06051992@gmail.com>
2015-03-19 10:00:27 +0700

    config gulp js successfully

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+var changed = require('gulp-changed');
+
+
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function () {
+   var files = [
+      '*.html',
+      'css/*.css',
+      'img/*.png',
+      'js/*.js'
+   ];
+
+   browserSync.init(files, {
+      server: {
+         baseDir: './'
+      }
+   });
+});
+
+
+// gulp.task('clean', function() {
+//     return gulp.src('build', {
+//             read: false
+//         })
+//         .pipe(clean());
+// });
+
+gulp.task('default', ['browser-sync']
+// 	, function() {
+//     gulp.watch('app/js/**/*.js', function(event) {
+//         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+//         gulp.src('app/js/**/*.js')
+//             .pipe(uglify())
+//             .pipe(concat('all.min.js'))
+//             .pipe(gulp.dest('build'));
+
+//         gulp.src('app/css/**/*.css')
+//             .pipe(uglify())
+//             .pipe(concat('all.min.css'))
+//             .pipe(gulp.dest('build'));
+//         console.log('Done');
+//     });
+// }
+);
