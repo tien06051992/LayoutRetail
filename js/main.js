51fcbfdd1e8c3ded31b00d0483c359217497cc47
chauhuynh2802 <chaucdth10a@gmail.com>
2015-03-24 09:31:18 +0700

    header and footer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+$(document).ready(function () {
+
+    // $(window).scroll(function () {
+    //     $(this).scrollTop() > 450 ?
+    //             $("#go-to-top").fadeIn() : $("#go-to-top").fadeOut()
+    // }),
+    //         $("#go-to-top").click(function () {
+    //     return $("body, html").animate({scrollTop: 0}, 400), !1
+    // })
+    $('.dropdown-toggle').dropdown()
+});
