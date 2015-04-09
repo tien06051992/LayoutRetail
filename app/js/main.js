c28e66dc61dd4adabe8ad7160f85a6956b1694dd
chauhuynh2802 <chaucdth10a@gmail.com>
2015-04-09 15:25:31 +0700

    my message

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,15 @@
 
 $(document).ready(function () {
 
-    $(window).scroll(function () {
-        $(this).scrollTop() > 450 ?
-                $("#go-to-top").fadeIn() : $("#go-to-top").fadeOut()
-    }),
-            $("#go-to-top").click(function () {
-        return $("body, html").animate({scrollTop: 0}, 400), !1
-    })
-    $('.dropdown-toggle').dropdown()
+    // $(window).scroll(function () {
+    //     $(this).scrollTop() > 450 ?
+    //             $("#go-to-top").fadeIn() : $("#go-to-top").fadeOut()
+    // }),
+    //         $("#go-to-top").click(function () {
+    //     return $("body, html").animate({scrollTop: 0}, 400), !1
+    // })
+    // $('.dropdown-toggle').dropdown()
+
+          $('.scroll-pane').jScrollPane();
+
 });
